a80c4983359ac50a25f55fa084997171ce9c49c8
jangerino <137700229+jangerino@users.noreply.github.com>
2025-08-09 15:14:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let result = document.getElementById("result");
 const websocketOutputDiv = document.getElementById("websocket-output");
 
 // Инициализация WebSocket
-const websocket = new WebSocket('ws://localhost:8765');
+const websocket = new WebSocket('wss://6zf71i9gr.localto.net');
 
 
 // Обработчики WebSocket
@@ -146,6 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     spinBtn.addEventListener('click', spinWheel);
 });
+
 
 
 
