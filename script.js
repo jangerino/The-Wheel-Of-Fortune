9aae2dc8507ca013631011f098c8e84352edd337
jangerino <137700229+jangerino@users.noreply.github.com>
2025-08-09 16:02:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const maxReconnectAttempts = 3;
 const reconnectDelay = 1000;
 
 function connectWebSocket() {
-    websocket = new WebSocket('wss://3sph1nsym.localto.net/');
+    websocket = new WebSocket('ws://3sph1nsym.localto.net:8765');
     
     websocket.onopen = () => {
         console.log('WebSocket подключен');
@@ -99,4 +99,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
